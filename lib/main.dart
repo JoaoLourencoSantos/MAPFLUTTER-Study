84138408cf62a9358e2e36e4ff1c4513c748c870
Santos <lourencosjm41@gmail.com>
2021-05-24 23:36:50 -0300

    refact

--- lib/main.dart
+++ lib/main.dart
@@ -30,17 +30,22 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late GoogleMapController mapController;
-  var geolocator = Geolocator();
 
-  static final LatLng _locationCenter =
+  static final LatLng _centerLocation =
       LatLng(-19.932532517135538, -43.938577202118765);
+
+  static final double _fixedZoom = 15.0;
+
+  static final String _urlMapStyle = "assets/map.json";
 
   void _onMapCreated(GoogleMapController controller) async {
     mapController = controller;
 
+    // Set style in map
     mapController.setMapStyle(
-        await DefaultAssetBundle.of(context).loadString('assets/map.json'));
+        await DefaultAssetBundle.of(context).loadString(_urlMapStyle));
 
+    // Set animation camera, to current position
     mapController.animateCamera(
         CameraUpdate.newCameraPosition(await _getCurrentPosition()));
   }
@@ -54,32 +59,32 @@ class _MyHomePageState extends State<MyHomePage> {
     Position position = await _getPosition();
 
     return CameraPosition(
-        target: LatLng(position.latitude, position.longitude), zoom: 15.0);
+        target: LatLng(position.latitude, position.longitude),
+        zoom: _fixedZoom);
   }
 
   CameraPosition _getInitialLocation() {
     return CameraPosition(
-        target: _locationCenter, zoom: 15.0, tilt: 0, bearing: 0);
+        target: _centerLocation, zoom: _fixedZoom, tilt: 0, bearing: 0);
   }
 
   Set<Marker> _createMarkers() {
     return {
-      Marker(
-        markerId: MarkerId("PUC_MINAS_CE"),
-        position: LatLng(-19.92257975766356, -43.99257354444666),
-        infoWindow: InfoWindow(title: 'PUC Minas - Coração Eucarístico'),
-      ),
-      Marker(
-        markerId: MarkerId("PUC_MINAS_SG"),
-        position: LatLng(-19.859143126099053, -43.91881268492343),
-        infoWindow: InfoWindow(title: 'PUC Minas - São Gabriel'),
-      ),
-      Marker(
-        markerId: MarkerId("PUC_MINAS_PL"),
-        position: LatLng(-19.933722065812983, -43.93633613095481),
-        infoWindow: InfoWindow(title: 'PUC Minas - Praça da Liberdade'),
-      ),
+      _buildMarker("PUC_MINAS_CE", "PUC Minas - Coração Eucarístico",
+          LatLng(-19.92257975766356, -43.99257354444666)),
+      _buildMarker("PUC_MINAS_SG", "PUC Minas - São Gabriel",
+          LatLng(-19.859143126099053, -43.91881268492343)),
+      _buildMarker("PUC_MINAS_PL", "PUC Minas - Praça da Liberdade",
+          LatLng(-19.933722065812983, -43.93633613095481)),
     };
+  }
+
+  Marker _buildMarker(String id, String title, LatLng location) {
+    return Marker(
+      markerId: MarkerId(id),
+      position: location,
+      infoWindow: InfoWindow(title: title),
+    );
   }
 
   @override
@@ -87,8 +92,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // This method is rerun every time setState is called, for i
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         backgroundColor: Color(0xFF158185),
       ),
